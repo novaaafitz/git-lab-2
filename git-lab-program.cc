19c1d4164fdfa9115418be836fb1a7b2b467062c
Nova Fitzmaurice <nf927617@ohio.edu>
2022-09-06 17:33:05 -0400

    Uploading practice for lab2

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nova Fitzmaurice
+ *        Date: 09/06/2022
+ * Description: Practice cpp program to practice updating in repository on github
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
